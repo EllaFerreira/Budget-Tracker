6e81289a6cdc38aea3061c864bc925389d7c8b7e
Ella <rafaela.morato1805@gmail.com>
2021-12-22 17:33:16 +1000

    testing

--- server.js
+++ server.js
@@ -16,12 +16,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budgettracker", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+// mongoose.connect("mongodb://localhost/budgettracker", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true,
+//   useFindAndModify: false,
+// });
 
 // routes
 app.use(require("./routes/api.js"));
